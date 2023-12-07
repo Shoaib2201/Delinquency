a590efe8e0537f2ab9f88f30ee436c98d0ca0cf6
zaheer shahzaib <zaheershahzaib0@gmail.com>
2023-12-07 23:05:39 +0500

    initial commit

---
+++ src/views/Dashboard.js
@@ -0,0 +1,276 @@
+/*!
+
+=========================================================
+* Black Dashboard React v1.2.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// react plugin used to create charts
+import { Line, Bar, Doughnut } from "react-chartjs-2";
+// reactstrap components
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Row,
+  Col,
+} from "reactstrap";
+
+// core components
+import {
+  chartExample1,
+  chartExample2,
+  chartExample3,
+  chartExample4,
+} from "variables/charts.js";
+
+function Dashboard(props) {
+  const [bigChartData, setbigChartData] = React.useState("data1");
+  const setBgChartData = (name) => {
+    setbigChartData(name);
+  };
+
+  const data = {
+    labels: ["Critical case", "Urgent case", "Errors", "Reviewed", "Success"],
+    datasets: [
+      {
+        data: [30, 30, 5, 15, 20],
+        backgroundColor: [
+          "#164863",
+          "#363062",
+          "#071952",
+          "#427D9D",
+          "#87295B"
+        ],
+        hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"]
+      }
+    ],
+
+    plugins: {
+      labels: {
+        render: "percentage",
+        fontColor: ["green", "white", "red"],
+        precision: 2
+      },
+    },
+    text: "23%",
+  };
+
+  return (
+    <>
+      <div className="content">
+        <Row>
+          <Col xs="12">
+            <Card className="card-chart">
+              <CardHeader>
+                <Row>
+                  <Col className="text-left" sm="6">
+                    <h5 className="card-category">Total Shipments</h5>
+                    <CardTitle tag="h2">Performance</CardTitle>
+                  </Col>
+                  <Col sm="6">
+                    <ButtonGroup
+                      className="btn-group-toggle float-right"
+                      data-toggle="buttons"
+                    >
+                      <Button
+                        tag="label"
+                        className={classNames("btn-simple", {
+                          active: bigChartData === "data1",
+                        })}
+                        color="info"
+                        id="0"
+                        size="sm"
+                        onClick={() => setBgChartData("data1")}
+                      >
+                        <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                          Accounts
+                        </span>
+                        <span className="d-block d-sm-none">
+                          <i className="tim-icons icon-single-02" />
+                        </span>
+                      </Button>
+                      <Button
+                        color="info"
+                        id="1"
+                        size="sm"
+                        tag="label"
+                        className={classNames("btn-simple", {
+                          active: bigChartData === "data2",
+                        })}
+                        onClick={() => setBgChartData("data2")}
+                      >
+                        <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                          Purchases
+                        </span>
+                        <span className="d-block d-sm-none">
+                          <i className="tim-icons icon-gift-2" />
+                        </span>
+                      </Button>
+                      <Button
+                        color="info"
+                        id="2"
+                        size="sm"
+                        tag="label"
+                        className={classNames("btn-simple", {
+                          active: bigChartData === "data3",
+                        })}
+                        onClick={() => setBgChartData("data3")}
+                      >
+                        <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                          Sessions
+                        </span>
+                        <span className="d-block d-sm-none">
+                          <i className="tim-icons icon-tap-02" />
+                        </span>
+                      </Button>
+                    </ButtonGroup>
+                  </Col>
+                </Row>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Line
+                    data={chartExample1[bigChartData]}
+                    options={chartExample1.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg="4">
+            <Card className="card-chart">
+              <CardHeader>
+                <h5 className="card-category">Total Shipments</h5>
+                <CardTitle tag="h3">
+                  <i className="tim-icons icon-bell-55 text-info" /> 763,215
+                </CardTitle>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Line
+                    data={chartExample2.data}
+                    options={chartExample2.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="4">
+            <Card className="card-chart">
+              <CardHeader>
+                <h5 className="card-category">Daily Sales</h5>
+                <CardTitle tag="h3">
+                  <i className="tim-icons icon-delivery-fast text-primary" />{" "}
+                  3,500€
+                </CardTitle>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Bar
+                    data={chartExample3.data}
+                    options={chartExample3.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="4">
+            <Card className="card-chart">
+              <CardHeader>
+                <h5 className="card-category">Completed Tasks</h5>
+                <CardTitle tag="h3">
+                  <i className="tim-icons icon-send text-success" /> 12,100K
+                </CardTitle>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Line
+                    data={chartExample4.data}
+                    options={chartExample4.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row >
+          <Col lg="4" >
+            <Card className="card-chart" style={{
+              height: '22rem'
+            }}>
+              <CardHeader>
+                <div className="chart-area" >
+                  <Doughnut
+                    data={data}
+                    options={{
+
+                      elements: {
+
+                        center: {
+                          legend: { display: true, position: "right" },
+                          text: "Red is 2/3 the total numbers",
+                          color: "#FF6384", // Default is #000000
+                          fontStyle: "Arial", // Default is Arial,
+                        }
+                      },
+
+                    }}
+                  />
+                </div>
+              </CardHeader>
+            </Card>
+          </Col>
+          <Col xl="8" >
+            <Card className="card-chart" style={{
+              height: '22rem'
+            }}>
+              <CardHeader>
+                <div className="chart-area" >
+                  <Line data={{
+                    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+                    datasets: [
+                      {
+                        label: "First dataset",
+                        data: [33, 53, 85, 41, 44, 65],
+                        fill: true,
+                        backgroundColor: "rgba(75,192,192,0.2)",
+                        borderColor: "rgba(75,192,192,1)"
+                      },
+                      {
+                        label: "Second dataset",
+                        data: [33, 25, 35, 51, 54, 76],
+                        fill: false,
+                        borderColor: "#742774"
+                      }
+                    ]
+                  }} />
+                </div>
+              </CardHeader>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
